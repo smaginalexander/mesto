b336d5239798635f39dfc55618efe27ab4780960
Alexandr Smagin <smaginalexander@yandex.ru>
2020-09-17 12:06:57 +0300

    9

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,10 +7,6 @@ export class Popup {
         this._popupSelector.classList.add('popup_opened');
         //нажатие на клавишу еск
         document.addEventListener('keydown', this._handleEscClose)
-        //клик на оверлей
-        document.addEventListener('mousedown', evt => {
-            if (evt.target.classList.contains('popup')) this.close();
-        });
     }
     close() {
         this._popupSelector.classList.remove('popup_opened');
